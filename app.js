8c9c51d6e2f9ba5ed073993531c53c6d541a1be6
Ulisses Alves <112980693+Ulisses22@users.noreply.github.com>
2024-04-26 03:36:26 +0100

    charge endpoint

--- app.js
+++ app.js
@@ -2,12 +2,39 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(bodyParser.json());
 
 
-app.listen(process.env.PORT, () =>{
+app.post('/charge', async (req, res) => {
+    const { amount, token } = req.body;
+
+    try {
+        const charge = await stripe.charges.create({
+            amount,
+            currency: 'usd',
+            source: token.id,
+            description: 'Chearge for test@example.com'
+        });
+
+        res.status(200).json({
+            status: 200,
+            message: 'Payment successful.'
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            status: 500,
+            message: 'Error creating charge.'
+        })
+    }
+})
+
+
+
+app.listen(process.env.PORT, () => {
 
     console.log(`Listening on ${process.env.PORT} ...`);
 })
